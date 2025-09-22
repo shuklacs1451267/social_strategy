4f0dbd4bd862c1c81faa91d72bee717fa6f4730a
Amit Shukla <shukla0132cs@gmail.com>
2025-09-23 03:39:59 +0530

    render SSL

--- server/utils/postgresDbExternal.js
+++ server/utils/postgresDbExternal.js
@@ -5,7 +5,10 @@ const postgresDbExternal = new Client({
     host: process.env.POSTGRES_HOST,  
     database: process.env.POSTGRES_DATABASE, 
     password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT
+    port: process.env.POSTGRES_PORT,
+    ssl: {
+    rejectUnauthorized: false
+  }
   });
 
   module.exports = postgresDbExternal;
