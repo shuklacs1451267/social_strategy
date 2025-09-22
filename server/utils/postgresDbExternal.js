aa86adac354439ec091e750feca829698c9970a7
Amit Shukla <shukla0132cs@gmail.com>
2025-09-23 03:50:53 +0530

    MM

--- server/utils/postgresDbExternal.js
+++ server/utils/postgresDbExternal.js
@@ -1,14 +1,17 @@
-const {Client} = require("pg")
-  console.log("HELOO",process.env.POSTGRES_USER)
+const { Client } = require("pg");
+require('dotenv').config();
+
 const postgresDbExternal = new Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,  
-    database: process.env.POSTGRES_DATABASE, 
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
-    ssl: {
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+  ssl: {
     rejectUnauthorized: false
   }
-  });
+});
 
-  module.exports = postgresDbExternal;
+postgresDbExternal.connect()
+  .then(() => console.log("Connected to Render Postgres!"))
+  .catch(err => console.error("Connection Error:", err));
