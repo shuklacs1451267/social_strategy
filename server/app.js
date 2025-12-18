9e2a58f9c45f2e72d724b4047e4c92dc3c92bd75
shuklacs1451267 <118541029+shuklacs1451267@users.noreply.github.com>
2025-12-18 09:00:58 +0000

    SEO

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const postgresDbExternal = require("./utils/postgresDbExternal")
 
 // Setup a global variable to resolve application dir path
 global.appDir = path.join(__dirname);
- 
+
 // Setup environment variable
 if (process.env.NODE_ENV === "production") {
   require("dotenv").config({ path: path.join(global.appDir, ".env") });
@@ -33,17 +33,56 @@ app.use(cors()) // enable cors
       parameterLimit: 5000,
     })
   );
-  console.log("ready")
+console.log("ready")
 
 // database connection
 
 postgresDbExternal.connect((err) => {
   if (err) {
-     return console.error("error", err)
-   } else {
-     console.log("connection stablish")
-   }
- })
+    return console.error("error", err)
+  } else {
+    console.log("connection stablish")
+  }
+})
+app.get("/sitemap.xml", (req, res) => {
+  res.header("Content-Type", "application/xml");
+
+  const baseUrl = "https://socialstech.com";
+  const today = new Date().toISOString().split("T")[0];
+
+  res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+  <url>
+    <loc>${baseUrl}/</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>${baseUrl}/about</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>${baseUrl}/services</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+
+  <url>
+    <loc>${baseUrl}/contact</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>yearly</changefreq>
+    <priority>0.6</priority>
+  </url>
+
+</urlset>`);
+});
 
 // Routers definitions files
 // const usersRouter = require("./routes/v1api/user");
